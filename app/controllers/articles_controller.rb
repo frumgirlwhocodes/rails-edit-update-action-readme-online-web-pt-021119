10653b926a9d767ebece0c542e5f4a647b3478eb
lieby weintraub <liebahinda@gmail.com>
2019-07-02 15:30:45 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,8 @@ class ArticlesController < ApplicationController
   
   def update 
    @article= Article.find(params[:id]) 
-   @article.update
+   @article.update(title: params[:article][:title], description: params[:article][:description])
+  redirect_to article_path(@article)
+end
   end 
 end
